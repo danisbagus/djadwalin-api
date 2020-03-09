af5c2e019308782ebb0f5f0ca49a4453b5510448
danisbagus <danisbagussetiawan@gmail.com>
2020-03-10 06:13:37 +0700

    Create lecturer services

--- services/index.js
+++ services/index.js
@@ -1,5 +1,7 @@
 const studentService = require("./student.services");
+const lecturerService = require("./lecturer.services");
 
 module.exports = {
-  studentService
+  studentService,
+  lecturerService
 };
